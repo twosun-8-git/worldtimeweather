af8db6d878a754a8af1458a091a5ff2c6091057e
Twosun8Git <cocominawork+twosun8git@gmail.com>
2025-02-22 00:58:15 +0900

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,28 @@ export default {
     extend: {
       colors: {
         background: "var(--background)",
+        "background-gray": "var(--background-gray)",
         foreground: "var(--foreground)",
+        white: "var(--white)",
+        blue: "var(--blue)",
+        red: "var(--red)",
+        "navy-100": "var(--navy-100)",
+        "gray-100": "var(--gray-100)",
+        "gray-200": "var(--gray-200)",
+        "gray-300": "var(--gray-300)",
+        "gray-500": "var(--gray-500)",
+        "gray-700": "var(--gray-700)",
+      },
+      screens: {
+        container: "940px",
+      },
+      maxWidth: {
+        "900": "900px",
+      },
+      fontSize: {
+        xs: "10px",
+        "10xl-vw": "18.6vw",
+        "10xl-fix": "198px",
       },
     },
   },
