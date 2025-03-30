78eb9d2d1e7eb93120c0bb6ffc4d2a62306c74c3
Twosun8Git <cocominawork+twosun8git@gmail.com>
2025-03-30 18:45:11 +0900

    フォームの送信内容の修正

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,8 +1,7 @@
-export const SITE_URL = "https://app.cocomina.com";
+export const SITE_URL = "https://worldtimeweather.com/";
 export const SITE_NAME = "World Time Weather";
 export const COMPANY_NAME = "CoComina";
 export const AUTHOR = "Tsukasa Chinen";
-export const EMAIL = "support@cocomina.com";
 export const LOCAL_STORAGE_KEY_ACTIVE_COUNTRY = "wtw_active_country";
 export const LOCAL_STORAGE_KEY_COUNTRIES = "wtw_countries";
 export const BREAKPOINTS = {
