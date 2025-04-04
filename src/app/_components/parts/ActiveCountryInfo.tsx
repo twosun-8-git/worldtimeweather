71b00964343acd8517ebf63b0adcf184f331d1cd
Twosun8Git <cocominawork+twosun8git@gmail.com>
2025-04-05 00:57:44 +0900

    update

--- src/app/_components/parts/ActiveCountryInfo.tsx
+++ src/app/_components/parts/ActiveCountryInfo.tsx
@@ -9,6 +9,7 @@ export function ActiveCotunryInfo() {
   const activeCountry = useAtomValue(activeCountryAtom);
 
   const { country, code, timezone } = activeCountry;
+
   return (
     <div className="flex items-start w-full justify-between md:items-center pc:px-1">
       <CountryTimezone country={country} code={code} timezone={timezone} />
