bfe0d5c1ebc0c1e45d716175825b4aad5c763539
Twosun8Git <cocominawork+twosun8git@gmail.com>
2025-04-16 00:24:41 +0900

    広告変更

--- src/app/_components/parts/Advertisement.tsx
+++ src/app/_components/parts/Advertisement.tsx
@@ -7,6 +7,19 @@ export function Advertisement() {
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADSENSE_CLIENT_ID}`}
         crossOrigin="anonymous"
       ></Script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client={process.env.GOOGLE_ADSENSE_CLIENT_ID}
+        data-ad-slot="8649972591"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <Script id="adsbygoogle">
+        {`
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        `}
+      </Script>
     </div>
   );
 }
